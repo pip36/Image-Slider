9088f6b551a8a6305eb15a308f566785e7fb73b7
pip36 <phil_23@live.co.uk>
2017-05-08 11:00:51 +0100

    refactored for reusability, and added messages to images

--- README.txt
+++ README.txt
@@ -1,9 +1,7 @@
-A Pen created at CodePen.io. You can find this one at http://codepen.io/pip36/pen/wdWYqr.
+A simple jquery looping image slider.
+----------------------------------------
 
- This is a simple JQuery looping image slider. 
-
-To add images just add them to the list enclosed in <li> tags, the javascript will take care of the rest.
-
-To adjust positioning of the slider, add css to the '.slider' class. Width value can be changed and will resize images, keeping their aspect ratio. The height value will set the height of the container div without affecting the image, so it may cause clipping off the bottom of tall images.
-
-To adjust length of time on each image, alter the number in the last line of javascript (setInterval), smaller values for less time.
+- Smooth transition for continuous looping.
+- Keeps the original images aspect ratio with a fixed width.
+- Clickable thumbnails to instantly view specific image
+- Message boxes to display image descriptions
--- js/index.js
+++ js/index.js
@@ -1,93 +1,121 @@
-var currentImage = 1;
-
-var Setup = function(){
-    var width = $('.slider').width();
-    var imageCount = $('.slider .images li').length;
- 
+function Slider(id){
+  var self = this;
+  this.id = id;
+  this.width = 0;
+  this.imageCount = 0;
+  this.currentImage = 1;
+  this.sliderSelector = $(this.id);
+  this.messages = [];
+  this.interval = setInterval(function(){self.slide()},3000);;
+//Call to initialize the slider
+  this.Setup = function(messageArr){
+    this.width = $(this.id).width();
+    this.imageCount = $(this.id + ' .images li').length;
+    this.messages = messageArr;
     //generate the thumbnail images underneath
- 
-  $('.slider ul').clone().removeClass("images").appendTo($('.thumbnails'));
-  
+    $(this.id + ' ul').clone().removeClass("images").appendTo(this.sliderSelector.next());
     //set the css for the ul
-    $('.slider .images').css({'list-style': 'none',
-                        'width': width * (imageCount + 2),
-                        'padding': 0,
-                        'margin-left': '0px' ,
-                        'position': 'relative'}); 
-   //and width for the list item
-    $('.slider .images li').css({'width': width, "display": "inline-block"});
-   
-  //and width of the images
-  $('.slider .images li img').css('width', 'inherit');
-  
+    $(this.id + ' .images').css({'list-style': 'none',
+                                 'width': this.width * (this.imageCount + 2),
+                                 'padding': 0,
+                                 'margin-left': '0px' ,
+                                 'position': 'relative'});
+    //and width for the list item
+    $(this.id + ' .images li').css({'width': this.width, "display": "inline-block"});
+    //and width of the images
+    $(this.id + ' .images li img').css('width', 'inherit');
+    //Clone first and last images for smooth looping
+    $(this.id + ' .images li').last().clone().prependTo($(this.id + ' .images'));
+    $(this.id + ' .images li').eq(1).clone().appendTo($(this.id + ' .images'));
+    $(this.id + ' .images li').css({"margin-right": "-5px"});
 
-  
-  //Clone first and last images for smooth looping
-  $('.slider .images li').last().clone().prependTo($('.slider .images'));
-  $('.slider .images li').eq(1).clone().appendTo($('.slider .images')); 
-  
-   $('.slider .images li').css({"margin-right": "-4px"});
-  setImage(currentImage);
+    //change image when thumbnails are clicked
+    this.sliderSelector.next().find("li").on("click", function(event){
+      self.setImage($(this).index() + 1);
+      clearInterval(self.interval);
+      self.interval = window.setInterval(function(){self.slide()},3000);
+    });
+    this.setImage(this.currentImage);
+  }
+
+//Moves directly to given image, no animation
+  this.setImage = function(index){
+    this.currentImage = index;
+    $(this.id + ' .images').css("left", -(this.width * index) + "px");
+    $(this.id + ' .img-message ').html("<p>" + this.messages[index-1] + "</p>");
+    this.updateThumbnails();
+  }
+
+//Update styling of the active thumbnail
+  this.updateThumbnails = function(){
+    this.sliderSelector.next().find("li").removeClass('active-thumb');
+
+    if (this.currentImage >= $(this.id + ' .images li').length -1){
+      this.sliderSelector.next().find("li").eq(0).addClass('active-thumb');
+    }
+    else if(this.currentImage <= -1){
+      this.sliderSelector.next().find("li").eq($(this.id + ' .images li').length - 2).addClass('active-thumb');
+    }
+    else{
+      this.sliderSelector.next().find("li").eq(this.currentImage - 1).addClass('active-thumb');
+    }
+  }
+
+//Slide to the next image and account for image wrapping
+  this.slide = function(){
+
+    //set 1 for forward scrolling, -1 for backward
+    this.currentImage += 1;
+    //Wrap the image when going beyond last image
+     if (this.currentImage >= $(this.id + ' .images li').length){
+      this.setImage(1);
+      this.slideToImage(2);
+      this.updateThumbnails();
+    }
+    //wrap when going less than first image
+    else if(this.currentImage <= -1){
+      this.setImage($(this.id + ' .images li').length - 2);
+      this.slideToImage($(this.id + ' .images li').length - 3);
+      this.updateThumbnails();
+    }
+    //slide to next as normal
+    else{
+      this.slideToImage(this.currentImage);
+      this.updateThumbnails();
+    }
+  }
+
+//Moves to given image with animation
+  this.slideToImage = function(index){
+    this.currentImage = index;
+    $(this.id + ' .images').animate({left:-(this.width * index) + "px"}, "slow");
+
+    if (this.currentImage >= $(this.id + ' .images li').length-1){
+     $(this.id + ' .img-message p').html("<p>" + this.messages[0] + "</p>");
+   }
+
+   else if(this.currentImage <= 0){
+     $(this.id + ' .img-message p').html("<p>" + this.messages[this.imageCount - 1] + "</p>");
+   }
+
+   else{
+     $(this.id + ' .img-message p').html("<p>" + this.messages[index-1] + "</p>");
+   }
+
+  }
 }
 
-//Sets the displayed image using the number given (no animation) 
-var setImage = function(index){
-  currentImage = index;
-  width = $('.slider').width();
-  $('.slider .images').css("left", -(width * index) + "px");
-  updateThumbnails();
-};
+//initialise a slider
+var slider1 = new Slider('#slider-1');
+slider1.Setup(["Cute Cat",
+              "Stupid Cat",
+              "Confused Cat",
+              "Cool Cat"]);
 
-//Animates to the image using the number given
-var slideToImage = function(index){
-  width = $('.slider').width();
-  $('.slider .images').animate({left:-(width * index) + "px"}, "slow");
-  currentImage = index;
-};
-
-var slide = function(){
-  currentImage += -1;
-  
-  //The if statement accounts for wrapping the image to make loop infinite
-   if (currentImage >= $('.slider .images li').length){
-    setImage(1);
-    slideToImage(2);
-     updateThumbnails();
-  }
-  else if(currentImage <= -1){
-    setImage($('.slider .images li').length - 2);
-    slideToImage($('.slider .images li').length - 2);
-     updateThumbnails();
-  }
-  else{
-    slideToImage(currentImage);
-     updateThumbnails();
-  }
- 
-};
-
-var updateThumbnails = function(){
-  $('.thumbnails ul li').removeClass('active-thumb');
-  
-  if (currentImage >= $('.slider .images li').length -1){
-    $('.thumbnails ul li').eq(0).addClass('active-thumb');
-  }
-  else if(currentImage <= -1){
-    $('.thumbnails ul li').eq($('.slider .images li').length - 2).addClass('active-thumb');
-  } 
-  else{
-    $('.thumbnails ul li').eq(currentImage - 1).addClass('active-thumb');
-  }
-}
-
-
-
-
-Setup();
-setInterval(function(){slide()},3000);
-
-
-
-   $('.thumbnails ul li').mousedown(function(event){
-    setImage($(this).index() + 1);
-});
+              var slider2 = new Slider('#slider-2');
+              slider2.Setup(["Cup Dog",
+                            "Ears Dog",
+                            "Car Dog",
+                            "Ball Dog",
+                            "Angry Dog",
+                            "Sleepy Dog"]);
